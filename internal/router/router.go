f645f4316e3b9f29b01aaa461b5c2fad48cde130
root <root@desktop.localdomain>
2021-09-25 17:47:56 +0800

    first run with empty demo

---
+++ internal/router/router.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "practice.com/blog_service/internal/router/api/v1"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	tagV1 := v1.NewTag()
+	articleV1 := v1.NewArticle()
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.POST("/tags", tagV1.Create)
+		apiv1.DELETE("/tags/:id", tagV1.Delete)
+		apiv1.PUT("/tags/:id", tagV1.Update)
+		apiv1.PATCH("/tags/:id/state", tagV1.Update)
+		apiv1.GET("/tags", tagV1.List)
+
+		apiv1.POST("/articles", articleV1.Create)
+		apiv1.DELETE("/articles/:id", articleV1.Delete)
+		apiv1.PUT("/articles/:id", articleV1.Update)
+		apiv1.PATCH("/articles/:id/state", articleV1.Update)
+		apiv1.GET("/articles/:id", articleV1.Get)
+		apiv1.GET("/articles", articleV1.List)
+	}
+
+	return r
+}
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"practice.com/blog_service/internal/router"
+	"time"
+)
+
 func main() {
 
+	routerHandler := router.NewRouter()
+
+	server := http.Server{
+		Addr:              ":8000",
+		Handler:           routerHandler,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
